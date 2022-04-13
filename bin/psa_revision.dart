a548386b89736d9642d8975d4674496acf48077a
Flavio Lucas Fernandes <flaviolukfer@gmail.com>
2022-04-13 19:44:07 -0300

    first commit

---
+++ bin/psa_revision.dart
@@ -0,0 +1,5 @@
+import 'package:psa_revision/psa_revision.dart' as psa_revision;
+
+void main(List<String> arguments) {
+  print('Hello world: ${psa_revision.calculate()}!');
+}
---
+++ test/psa_revision_test.dart
@@ -0,0 +1,8 @@
+import 'package:psa_revision/psa_revision.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
